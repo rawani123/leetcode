11618638047844f8cae424fd396a592799a13733
Aniruddha Rawool <121213641+rawani123@users.noreply.github.com>
2024-07-06 13:57:16 +0530

    Time: 18 ms (14.16%) | Memory: 12.9 MB (10.87%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+       unordered_map<char,int> mpp={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}} ;
+    
+    int index=0;
+    int sum=0;
+    while(index<s.size()){
+        if(mpp[s[index]]<mpp[s[index+1]]){
+            sum-=mpp[s[index]];
+            index++;
+        }else{
+            sum+=mpp[s[index]];
+            index++;
+        }
+    }
+    return sum;
+
+    };
+
+};
