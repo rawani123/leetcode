cb26f7dd3ce131af6730382d00a8585ea0e747c1
Aniruddha Rawool <121213641+rawani123@users.noreply.github.com>
2024-07-06 16:54:33 +0530

    Time: 97 ms (44.80%) | Memory: 73.1 MB (44.48%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -3,13 +3,12 @@ public:
     bool containsDuplicate(vector<int>& nums) {
        unordered_map<int,int> mpp;
         for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;
-        }
-        for(int i=0;i<nums.size();i++){
-            if(mpp[nums[i]]>1){
+            if(mpp[nums[i]]>=1){
                 return true;
             }
+            mpp[nums[i]]++;
         }
+        
         return false; 
     }
 };
