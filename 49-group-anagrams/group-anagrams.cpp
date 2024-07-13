8fa5629087fa9cb7cafb411734bd4d7c58af8497
Aniruddha Rawool <121213641+rawani123@users.noreply.github.com>
2024-07-13 14:52:48 +0530

    Time: 22 ms (86.71%) | Memory: 25.2 MB (24.76%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -4,14 +4,14 @@ public:
         
         unordered_map<string, vector<string>> mp;  
         
-        for (string& s : strs) {  
+        for (string s : strs) {  
             string key = s;     
             sort(key.begin(), key.end());           
             mp[key].push_back(s);
         }
         
         vector<vector<string>> groupedAnagrams;
-        for (auto& kv : mp) {
+        for (auto kv : mp) {
             groupedAnagrams.push_back(kv.second); 
         }
         
