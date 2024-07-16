1084a122654cee61a7f1ea532282fc3e08416520
Aniruddha Rawool <121213641+rawani123@users.noreply.github.com>
2024-07-16 23:42:09 +0530

    Time: 42 ms (41.63%) | Memory: 33.7 MB (13.63%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    int findDays(vector<int>& weights, int cap){
-        long load=0;
-        int days=1;
-        for(int i=0;i<weights.size();++i){
-            if(weights[i]+load>cap){
-                days+=1;
-                load=weights[i];
-            }
-            else{
-                load+=weights[i];
+    int findDays(const vector<int>& weights, int cap) {
+        int days = 1;
+        int load = 0;
+        for (int weight : weights) {
+            if (load + weight > cap) {
+                days++;
+                load = weight;
+            } else {
+                load += weight;
             }
         }
         return days;
     }
+
     int shipWithinDays(vector<int>& weights, int days) {
-        int low = *max_element(weights.begin(),weights.end());
-        long high= accumulate(weights.begin(),weights.end(),0);
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            int noOfDays=findDays(weights,mid);
-            if(noOfDays<=days){
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
+        int low = *max_element(weights.begin(), weights.end());
+        int high = accumulate(weights.begin(), weights.end(), 0);
+        
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (findDays(weights, mid) <= days) {
+                high = mid;
+            } else {
+                low = mid + 1;
             }
         }
+        
         return low;
     }
 };
