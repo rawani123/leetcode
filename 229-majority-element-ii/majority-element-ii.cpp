ffdc46530bd56e094f544c08600b04ba4478a853
Aniruddha Rawool <121213641+rawani123@users.noreply.github.com>
2024-09-25 15:35:08 +0530

    Time: 10 ms (48.86%) | Memory: 18.8 MB (40.85%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,40 +1,18 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-    int count1=0,count2=0;
-    int el1=0,el2=1;  
-    for(int i=0;i<nums.size();i++){
-        if(count1==0 && nums[i]!=el2){ 
-            count1=1;     
-            el1=nums[i]; 
+    map<int,int> mpp;
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]]++;
         }
-        else if(count2==0 && nums[i]!=el1){
-            count2=1; 
-            el2=nums[i]; 
-        }else if(el1==nums[i]){
-            count1++; 
+        vector<int> majorityElements;
+        for(int i=0;i<nums.size();i++){
+            if(mpp[nums[i]]>nums.size()/3){
+                if(majorityElements.size()<2){
+                    if((majorityElements.empty() || majorityElements[0]!=nums[i]) ) majorityElements.push_back(nums[i]);
+                }
+            }
         }
-        else if(el2==nums[i]){
-            count2++;
-        }
-        else{  
-            count1--;
-            count2--;
-        }
-    }
-
-    vector<int> ans;
-    count1=0;   
-    count2=0;
-    for(int i=0;i<nums.size();i++){
-        if(nums[i]==el1) count1++; 
-        if(nums[i]==el2) count2++; 
-    }
-    int maxtimes=(int)(nums.size()/3)+1;
-
-    if(count1>=maxtimes) ans.push_back(el1);
-    if(count2>=maxtimes) ans.push_back(el2);
-
-    return ans;
+    return majorityElements; 
 }
 };
